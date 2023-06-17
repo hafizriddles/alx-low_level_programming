33e00973212d15df619ebf030825f42f7ed5d04a
Hafiz Yakubu <yakubuhafiz40@gmail.com>
2023-06-17 11:03:23 -0700

    Write required scripts and make files executable or compile iwith gcc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+* main - prints the string in the puts function
+* Description: using the main puts, this program prints
+* "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
