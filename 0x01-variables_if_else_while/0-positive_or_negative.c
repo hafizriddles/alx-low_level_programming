93dcb8813b5e3b76ddeadc2eda707e7ffd97b924
Hafiz Yakubu <yakubuhafiz40@gmail.com>
2023-06-19 17:45:00 -0700

    Create README.md and all necessary files. Also, create source code for 0 and compile with gcc

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Description: Using the if and else if commands
+ * This program prints whether a random number generated is
+ * positve, zero or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This README regards the tasks under 0x01-variables_if_else_while
